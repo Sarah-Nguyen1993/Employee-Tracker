73194ebc896a00319178b981cb8534ca74643ba8
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-25 18:00:36 -0500

    added codes to create department, role, and employees

---
+++ createNew.js
@@ -0,0 +1,35 @@
+function createDepartment (connection, data){
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO department SET ?", data, 
+        function(err, data){
+            if(err){ reject(err) }
+            else { resolve(data) }
+        });
+    })
+};
+
+function createRole (connection, data){
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO role SET ?", data, 
+        function(err, data){
+            if(err){ reject(err) }
+            else { resolve(data) }
+        });
+    })
+};
+
+function createEmployees (connection, data){
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO employees SET ?", data, 
+        function(err, data){
+            if(err){ reject(err) }
+            else { resolve(data) }
+        });
+    })
+};
+
+module.exports = {
+    createDepartment,
+    createRole,
+    createEmployees
+}
