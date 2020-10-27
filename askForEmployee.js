00b1a87e571e6a08e5176817299161d5ea5f3044
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-26 20:44:18 -0500

    created to move codes from index.js to here

---
+++ askForEmployee.js
@@ -0,0 +1,40 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const employeeList = require("./dataList/employeeList");
+const roleList = require("./dataList/roleList");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bootcampUT2020",
+    database: "employee_trackerBD"
+});
+async function askForEmployee(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name:"first_name",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name:"last_name",
+            message: "What is the employee's last name?"
+        },
+        {
+            type: "list",
+            name:"role_id",
+            message: "What is the employee's role?",
+            choices: await roleList(connection)
+        },
+        {
+            type: "list",
+            name:"manager_id",
+            message: "Who does the role report to?",
+            choices: await employeeList(connection)
+        },
+    ])
+};
+
+module.exports = askForEmployee;
---
+++ askForRole.js
@@ -0,0 +1,34 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const departmentList = require("./dataList/departmentList");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bootcampUT2020",
+    database: "employee_trackerBD"
+});
+async function askForRole(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What role would you like to add?"
+        },
+        {
+            type: "list",
+            name: "department_id",
+            message: "What department does the role belong to?",
+            choices: await departmentList(connection)
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary of the role?"
+        }
+
+    ])
+};
+
+module.exports = askForRole;
