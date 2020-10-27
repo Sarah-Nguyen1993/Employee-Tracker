baa96945307f827d0115baddf83c977e513b0abe
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-26 20:43:09 -0500

    modified codes to make the display look cleaner

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const {createDepartment, createRole, createEmployees} = require("./createNew");
-const {departmentList, employeeList, roleList} = require ("./view")
-const askMenu = require("./askMenu")
+const departmentList = require("./dataList/departmentList");
+const askMenu = require("./askMenu");
+const askForRole = require("./askForRole");
+const askForDepartment = require("./askForDepartment");
+const askForEmployee = require("./askForEmployee");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -21,93 +24,36 @@ connection.connect(function(err) {
 async function start(){
     const {menu} = await askMenu();
     if (menu === "Add department"){
-        addDepartmentFlow();
+        addDepartment();
     }
     else if (menu === "Add roles"){
-        addRoleFlow();
+        addRole();
     }
     else if (menu === "Add employees"){
-        addEmployeeFlow();
+        addEmployee();
     }
     else if (menu === "View department"){
         await viewDepartmentList();
     }
 }
 
-async function addDepartmentFlow(){
-    const answer = await addDepartment();
+async function addDepartment(){
+    const answer = await askForDepartment();
     await createDepartment(connection, answer);
     console.log(`${answer.name} is added to the database.`)
 };
 
-function addDepartment(){
-    return inquirer.prompt({
-        type: "input",
-        name: "name",
-        message: "What department would you like to add?"
-    })
-}
-async function addRoleFlow(){
-    const answer = await addRole();
-    //console.log(title, departmentName,salary);
+async function addRole(){
+    const answer = await askForRole();
     await createRole(connection, answer);
     console.log(`${answer.title} is added to the database.`)
 };
 
-async function addRole(){
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "What role would you like to add?"
-        },
-        {
-            type: "list",
-            name: "department_id",
-            message: "What department does the role belong to?",
-            choices: await departmentList(connection)
-        },
-        {
-            type: "input",
-            name: "salary",
-            message: "What is the salary of the role?"
-        }
-
-    ])
-}
-async function addEmployeeFlow(){
-    const answer = await addEmployee();
+async function addEmployee(){
+    const answer = await askForEmployee();
     await createEmployees(connection, answer);
     console.log(`${answer.first_name} ${answer.last_name} is added to the database.`)
 };
-
-
-async function addEmployee(){
-    return inquirer.prompt([
-        {
-            type: "input",
-            name:"first_name",
-            message: "What is the employee's first name?"
-        },
-        {
-            type: "input",
-            name:"last_name",
-            message: "What is the employee's last name?"
-        },
-        {
-            type: "list",
-            name:"role_id",
-            message: "What is the employee's role?",
-            choices: await roleList(connection)
-        },
-        {
-            type: "list",
-            name:"manager_id",
-            message: "Who does the role report to?",
-            choices: await employeeList(connection)
-        },
-    ])
-}
 
 async function viewDepartmentList(){
     const departmentArray =  await departmentList(connection);
