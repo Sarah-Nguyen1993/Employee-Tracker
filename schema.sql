8b2787fd2130ae98eb96ecb73ecb3b6f7a8a5de9
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-27 18:07:43 -0500

    updated IDs to be not null

--- schema.sql
+++ schema.sql
@@ -3,12 +3,12 @@ Create Database employee_trackerBD;
 Use employee_trackerBD;
 
 Create Table department (
-    id INT PRIMARY KEY AUTO_INCREMENT, 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     name VARCHAR (30)
 );
 
 Create Table role (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title VARCHAR (30),
     salary DECIMAL,
     department_id INT,
@@ -16,7 +16,7 @@ Create Table role (
 );
 
 Create Table employees (
-    id INT PRIMARY KEY AUTO_INCREMENT, 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT,
