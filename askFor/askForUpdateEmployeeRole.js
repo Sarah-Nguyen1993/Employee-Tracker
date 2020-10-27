9cdfdffb6bfd750013493bb7396c4c9d776c70de
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-26 22:52:32 -0500

    created new file

---
+++ askFor/askForUpdateEmployeeRole.js
@@ -0,0 +1,23 @@
+const inquirer = require("inquirer");
+const employeeList = require("../dataList/employeeList");
+const roleList = require("../dataList/roleList");
+
+async function askForUpdateEmployeeRole(connection){
+    return inquirer.prompt(
+        {
+            type:"list",
+            name:"name",
+            message:"Which employee do you want to update role?",
+            choices: await employeeList(connection)
+        },
+        {
+            type:"list",
+            name:"role_id",
+            message:"What new role is the employee assigned to?",
+            choices: await roleList(connection)
+        },
+       
+    )
+};
+
+module.exports = askForUpdateEmployeeRole;
