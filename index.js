6a74cfa981f43fb3439d1b7b1e5f00d3f91a54e7
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-26 22:53:00 -0500

    added codes to update employee role

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const {createDepartment, createRole, createEmployees} = require("./createNew");
-const departmentList = require("./dataList/departmentList");
 const askForMenu = require("./askFor/askForMenu");
 const askForRole = require("./askFor/askForRole");
 const askForDepartment = require("./askFor/askForDepartment");
 const askForEmployee = require("./askFor/askForEmployee");
+const askForUpdateEmployeeRole = require("./askFor/askForUpdateEmployeeRole");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -37,6 +37,12 @@ async function start(){
     }
     else if (menu === "View roles"){
         await viewRoles();
+    }
+    // else if (menu === "View all employees"){
+    //     await viewAllEmployees();
+    // }
+    else if (menu === "Update employee role"){
+        await updateEmployeeRole();
     }
 }
 
@@ -73,7 +79,7 @@ function viewDepartments(){
 
 function viewRoles(){
     return new Promise((resolve, reject) => {
-        connection.query("SELECT role.title, role.salary, department.name AS department FROM role INNER JOIN department ON role.department_id = department.id ORDER BY salary DESC",
+        connection.query("SELECT role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id ORDER BY salary DESC",
             function (err, data) {
                 if (err) { reject(err) }
                 else {
@@ -84,4 +90,39 @@ function viewRoles(){
     })
 }
 
+// function viewAllEmployees(){
+//     var query = "SELECT employees.id, employees.first_name, employees.last_name,"
+//     query += "role.title, department.name as department, role.salary, CONCAT(employee.first_name, ' ', employee.last_name) as manager employees.manager_id ";
+//     query += " FROM employees";
+//     query += " INNER JOIN role on employees.role_id = role.id";
+//     query += " INNER JOIN department on role.department_id = department.id";
+//     query += " LEFT JOIN employee AS manager on employee.manager_id = department.id";
+//     query += " ORDER BY salary DESC"
+//     return new Promise((resolve, reject) => {
+//         connection.query(query,
+//             function (err, data) {
+//                 if (err) { reject(err) }
+//                 else {
+//                     console.table(data)
+//                     resolve(data);
+//                 }
+//             });
+//     })
+// }
+
+async function updateEmployeeRole(){
+    console.log("fsa")
+    const answer = await askForUpdateEmployeeRole(connection);
+    console.log(answer)
+    // return new Promise((resolve, reject) => {
+    //     connection.query("UPDATE employee SET WHERE role_id= ?",[role]
+    //         function (err, data) {
+    //             if (err) { reject(err) }
+    //             else {
+    //                 console.table(data)
+    //                 resolve(data);
+    //             }
+    //         });
+    // })
+}
 
