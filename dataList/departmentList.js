eb89cac8f2ee2c958f4b8e93ed6556003705f5e8
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-27 23:01:29 -0500

    deleted managerlist that was unintentionally added

--- dataList/departmentList.js
+++ dataList/departmentList.js
@@ -11,17 +11,4 @@ function departmentList(connection) {
     })
 };
 
-function managerList(connection) {
-    return new Promise((resolve, reject) => {
-        const managers = connection.query("SELECT CONCAT(first_name, ' ', last_name) from employees where employees.id = employees.manager_id",
-            function (err, data) {
-                if (err) { reject(err) }
-                else {
-                    console.log(managers)
-                    resolve(data);
-                }
-            });
-    })
-};
-
-module.exports = {departmentList,managerList};
+module.exports = departmentList;
