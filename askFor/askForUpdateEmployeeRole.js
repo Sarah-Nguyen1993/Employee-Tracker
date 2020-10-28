26e360b958b9d235f35847d1cff690e41f667e12
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-27 23:34:43 -0500

    updated

--- askFor/askForUpdateEmployeeRole.js
+++ askFor/askForUpdateEmployeeRole.js
@@ -1,21 +1,31 @@
 const inquirer = require("inquirer");
-const employeeList = require("../dataList/employeeList");
+const departmentList = require("../dataList/departmentList");
 const roleList = require("../dataList/roleList");
 
 async function askForUpdateEmployeeRole(connection){
     return inquirer.prompt([
         {
             type:"list",
-            name:"name",
-            message:"Which employee do you want to update role?",
-            choices: await employeeList(connection)
+            name:"role_id",
+            message:"What role do you want to update?",
+            choices: await roleList(connection)
         },
         {
-            type:"list",
-            name:"role_id",
-            message:"What is the new role of this employee?",
-            choices: await roleList(connection)
-        }   
+            type:"input",
+            name:"newRole",
+            message:"What is the new role?"
+        },
+        {
+            type: "list",
+            name: "department_id",
+            message: "What department does the role belong to?",
+            choices: await departmentList(connection)
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary of the role?"
+        }
     ])
 };
 
